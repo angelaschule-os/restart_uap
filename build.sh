55c8cde540541afe0c29b0e3581eb3f2b5801a3f
BjoernSchilberg <bjoern.schilberg@posteo.de>
2023-03-19 23:40:00 +0100

    Make binary a static executable.

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+pip install -r requirements.txt pyinstaller staticx
+
 # Get the Git version hash
 git_hash=$(git rev-parse --short HEAD)
 
@@ -8,3 +10,7 @@ echo "GIT_HASH = '${git_hash}'" > git_version.py
 
 # Build the binary with PyInstaller
 pyinstaller --onefile --name restart_uap main.py
+
+# Bundle dynamic executables with their library dependencies so they can be run
+# anywhere, just like a static executable.
+staticx dist/restart_uap dist/restart_uap
